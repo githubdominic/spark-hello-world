5a120c39c359866fafb10c7526285a0736005aaf
dominic <w11892708@163.com>
2019-02-16 23:19:58 +0800

    Spark 'Hello World'

---
+++ spark-hello-world/src/main/java/spark/spark_hello_world/App.java
@@ -0,0 +1,48 @@
+package spark.spark_hello_world;
+
+
+/**
+ * Hello Spark!
+ *
+ */
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Computes an approximation to pi
+ * Usage: JavaSparkPi [partitions]
+ */
+public final class App {
+
+  public static void main(String[] args) throws Exception {
+
+
+    SparkConf sparkConf = new SparkConf().setAppName("JavaSparkPi").setMaster("local");
+    
+    JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+
+    int slices = (args.length == 1) ? Integer.parseInt(args[0]) : 2;
+    int n = 100000 * slices;
+    List<Integer> l = new ArrayList<>(n);
+    for (int i = 0; i < n; i++) {
+      l.add(i);
+    }
+
+    JavaRDD<Integer> dataSet = jsc.parallelize(l, slices);
+
+    int count = dataSet.map(integer -> {
+      double x = Math.random() * 2 - 1;
+      double y = Math.random() * 2 - 1;
+      return (x * x + y * y <= 1) ? 1 : 0;
+    }).reduce((integer, integer2) -> integer + integer2);
+
+    System.out.println("Pi is roughly " + 4.0 * count / n);
+
+    jsc.stop();
+  }
+}
